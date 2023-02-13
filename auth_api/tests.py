10435678ff9576fc919cf1829d0636d93ce9e2d9
Fer <fernanda.ht1998@gmail.com>
2023-02-13 16:00:28 -0600

    Addition of test

--- auth_api/tests.py
+++ auth_api/tests.py
@@ -1,5 +1,6 @@
 import unittest
 from methods import Token, Restricted
+import requests
 
 
 class TestStringMethods(unittest.TestCase):
@@ -8,12 +9,37 @@ class TestStringMethods(unittest.TestCase):
         self.convert = Token()
         self.validate = Restricted()
         
-    #Commented this test since the token will always change
-    # def test_generate_token(self):
-    #    self.assertEqual('eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6ImFkbWluIiwicm9sZSI6ImFkbWluIiwiZXhwIjoxNjc2MDU2NDI3fQ.lMe44mQKPbucFFI_onFfDErahtpCcbaVdoMpb3gahkw', self.convert.generate_token('admin', 'secret'))
+    def test_generate_token_admin(self):
+        payload = {'username': 'admin', 'password':'secret'}
+        response = requests.post('http://localhost:8000/login', data=payload)
+        self.assertEqual(response.status_code, 200)
+
+    def test_generate_token_noadmin(self):
+        payload = {'username': 'noadmin', 'password':'noPow3r'}
+        response = requests.post('http://localhost:8000/login', data=payload)
+        self.assertEqual(response.status_code, 200)
+    
+    def test_generate_token_bob(self):
+        payload = {'username': 'bob', 'password':'thisIsNotAPasswordBob'}
+        response = requests.post('http://localhost:8000/login', data=payload)
+        self.assertEqual(response.status_code, 200)
+
+    def test_generate_token_error(self):
+        payload = {'username': 'fer', 'password':'pass'}
+        response = requests.post('http://localhost:8000/login', data=payload)
+        self.assertEqual(response.status_code, 403)
 
     def test_access_data(self):
-        self.assertEqual('You are under protected data', self.validate.access_data('eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6ImFkbWluIiwicm9sZSI6ImFkbWluIiwiZXhwIjoxNjc2MDU2MjQ1fQ.lBF79m4mjcqPI3f60xfotpm8hGLAvyRXZ5W_ptVFZKc'))
+        self.assertEqual('You are under protected data', self.validate.access_data('eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6ImFkbWluIiwicm9sZSI6ImFkbWluIiwiZXhwIjoxNjc2MzI3NTYyfQ.gnOiGLU81Uouj4kZiyUH-IFlCWSysb7GoZxSawK0_Ek'))
+
+    def test_access_data_check(self):
+        payload = {'Authorization':'eyJhbGciOiJIUzI1NiIsInR5cfCI6IkpXVCJ9.eyJ1cs2VybmFtZSI6ImFkbWluIiwicm9sZSI6ImFkbWluIiwiZXhwIjoxNjc2MzI3NTYyfQ.gnOiGLU81Uouj4kZiyUH-IFlCWSysb7GoZxSawK0_Ekdf'}
+        url = 'http://localhost:8000/protected'
+        response = requests.get(url, headers=payload)
+        self.assertEqual(response.status_code, 200)
+    
+    def test_access_data_invalid(self):
+        self.assertEqual('Invalid Token', self.validate.access_data('eyJhbGciOiJIdUzI1NiIsghInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6ImFkbWluIiwicm9sZSI6ImFkbWluIiwiZXhwIjoxNjc2MzI3NTYyfQ.gnOiGLU81Uouj4kZiyUH-IFlCWSysb7GoZxSawK0_Ek'))
 
 if __name__ == '__main__':
     unittest.main()
